92645d63916d2fe8cab25c756a03254dbebe29b3
iamkevingreen <kevin@redantler.com>
2017-01-26 17:39:50 -0500

    modify the production config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,4 +1,39 @@
-var config = require('./webpack.config.js');
-config.plugins.shift();
-config.output.publicPath = './dist/';
+// webpack.config.production.js
+var path = require('path');
+var webpack = require('webpack');
+var config = {
+    context: __dirname + '/src',
+    entry: {
+      app: ['webpack/hot/dev-server', './app.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js',
+        publicPath: './build/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: "json-loader"
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style!css!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded'
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
+
 module.exports = config;
