438e3820aeb38229da28f7ce3d23728a0e71cbd6
iamkevingreen <kevin@redantler.com>
2017-01-27 16:47:58 -0500

    hook up the webpack production config to be a lot nicer

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,39 +1,5 @@
-// webpack.config.production.js
-var path = require('path');
-var webpack = require('webpack');
-var config = {
-    context: __dirname + '/src',
-    entry: {
-      app: ['./app.js']
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js',
-        publicPath: './build/'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            },
-            {
-                test: /\.json$/,
-                loader: "json-loader"
-            },
-            {
-                test: /\.scss$/,
-                loader: 'style!css!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded'
-            }
-        ]
-    },
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map'
-};
-
+var config = require('./webpack.config.js');
+config.entry.app.shift();
+config.plugins.shift();
+config.output.publicPath = './build/';
 module.exports = config;
